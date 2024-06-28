c22e54e70bed401f61f3eceb5ed5b6427a751724
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2024-06-28 10:42:14 +0530

    Update index.jsx

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,7 +1,7 @@
 // import create from zustand
 import { create } from "zustand";
 
-
+// created the AppStore for updateTheme
 export const AppStore = create((set) => ({
     theme:'light',
     updateTheme: data => set(state => ({ theme: data })),
